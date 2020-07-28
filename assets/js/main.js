66fd3f4836450d2b964e476eb116a29438ebe9b1
vainawebm <vainaweb@gmail.com>
2020-07-28 16:01:52 -0300

    exercício 1 terminado

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// 1 - Criar um contador de 0 a 10 usando for //
+
+for (let number = 1; number <= 10; number++) {
+    console.log(number);
+}
+
+// 2 - Criar um contador de 10 a 0 usando while //
+
+// 3 - Contar todos os numeros pares de 0 a 100 usando for //
+
+// 4 - Conrar todos os numeros ímpares de 0 a 100 usando while //
+
+// 5 - Logar uma lista usando Map e forEach //
